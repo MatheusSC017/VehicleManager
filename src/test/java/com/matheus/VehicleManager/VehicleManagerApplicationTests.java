f2fa89a9d26f8b7576e75a7dcf08ab5e97b711fb
MatheusSC017 <matheussimao2101@gmail.com>
2025-05-03 10:51:13 -0300

    Initial commit

---
+++ src/test/java/com/matheus/VehicleManager/VehicleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.VehicleManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
