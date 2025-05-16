772164dde56d907ee6531a19de70ae7d5b2b836a
MatheusSC017 <matheussimao2101@gmail.com>
2025-05-16 08:18:54 -0300

    Including edit option using the same form

--- src/main/java/com/matheus/VehicleManager/controller/VehicleController.java
+++ src/main/java/com/matheus/VehicleManager/controller/VehicleController.java
@@ -39,19 +39,19 @@ public class VehicleController {
     }
 
     @GetMapping("/veiculos/cadastrar")
-    public ModelAndView getInsertVehicleForm(Vehicle vehicle) {
+    public ModelAndView getInsertForm(Vehicle vehicle) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("vehicles/vehicle_register");
+        modelAndView.setViewName("vehicles/vehicle_form");
         modelAndView.addObject("vehicle", new Vehicle());
         return modelAndView;
     }
 
     @PostMapping("/veiculos/cadastrar")
-    public ModelAndView insertVehicle(@Valid Vehicle vehicle, BindingResult bindingResult) {
+    public ModelAndView insert(@Valid Vehicle vehicle, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("vehicles/vehicle_register");
-            modelAndView.addObject("vehicle");
+            modelAndView.setViewName("vehicles/vehicle_form");
+            modelAndView.addObject("vehicle", vehicle);
         } else {
             modelAndView.setViewName("redirect:/veiculos");
             vehicleRepository.save(vehicle);
@@ -59,14 +59,29 @@ public class VehicleController {
         return modelAndView;
     }
 
-    @GetMapping("/veiculos/editar/{id}")
-    public ModelAndView update() {
+    @GetMapping("/veiculos/{id}/editar")
+    public ModelAndView getUpdateForm(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("vehicles/vehicles");
+        modelAndView.setViewName("vehicles/vehicle_form");
+        Vehicle vehicle = vehicleRepository.getReferenceById(id);
+        modelAndView.addObject("vehicle", vehicle);
         return modelAndView;
     }
 
-    @GetMapping("/veiculos/editar/{id}/delete")
+    @PostMapping("/veiculos/{id}/editar")
+    public ModelAndView update(@Valid Vehicle vehicle, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("vehicles/vehicle_form");
+            modelAndView.addObject("vehicle", vehicle);
+        } else {
+            modelAndView.setViewName("redirect:/veiculos/" + vehicle.getId());
+            vehicleRepository.save(vehicle);
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/veiculos/{id}/delete")
     public ModelAndView delete() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("vehicles/vehicles");
