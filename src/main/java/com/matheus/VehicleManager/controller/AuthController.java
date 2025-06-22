fac516151b94450152beb59f23a7870dabf31712
MatheusSC017 <matheussimao2101@gmail.com>
2025-06-22 13:07:11 -0300

    Including route to register user

--- src/main/java/com/matheus/VehicleManager/controller/AuthController.java
+++ src/main/java/com/matheus/VehicleManager/controller/AuthController.java
@@ -4,19 +4,33 @@ import com.matheus.VehicleManager.dto.AuthRequest;
 import com.matheus.VehicleManager.dto.AuthResponse;
 import com.matheus.VehicleManager.security.CustomUserDetailsService;
 import com.matheus.VehicleManager.security.JwtUtil;
+import com.matheus.VehicleManager.model.User;
+import com.matheus.VehicleManager.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -39,5 +53,28 @@ public class AuthController {
         } else {
             throw new RuntimeException("Invalid credentials");
         }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, Object> response = new HashMap<>();
+            user.setPassword("");
+            response.put("content", user);
+
+            Map<String, String> errors = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(error ->
+                    errors.put(error.getField(), error.getDefaultMessage())
+            );
+            response.put("errors", errors);
+
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.saveUser(user);
+
+        return ResponseEntity.ok("User registered");
     }
 }
