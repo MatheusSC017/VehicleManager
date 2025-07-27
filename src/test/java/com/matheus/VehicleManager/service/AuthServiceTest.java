c0b9adebb84d3eb51e1be479eead1dddd8a8a8e9
MatheusSC017 <matheussimao2101@gmail.com>
2025-07-27 09:28:50 -0300

    Including tests to auth service

---
+++ src/test/java/com/matheus/VehicleManager/service/AuthServiceTest.java
@@ -0,0 +1,52 @@
+package com.matheus.VehicleManager.service;
+
+import com.matheus.VehicleManager.enums.UserRole;
+import com.matheus.VehicleManager.model.User;
+import com.matheus.VehicleManager.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AuthServiceTest {
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void TestCreateUser() {
+        String password = "TestPassword";
+        String encodedPassword = "EncodedPassword";
+        User user = new User();
+        user.setUsername("TestUsername");
+        user.setPassword(password);
+        user.setRole(UserRole.USER);
+
+        when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User createdUser = authService.createUser(user);
+
+        assertEquals(user, createdUser);
+        assertEquals(user.getPassword(), encodedPassword);
+        verify(passwordEncoder, times(1)).encode(password);
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+}
